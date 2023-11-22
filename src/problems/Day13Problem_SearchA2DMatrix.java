c7c9594b8a393e1813e2171ac8dd7241ff605b89
Hemesh10 <hemesht97@gmail.com>
2023-11-22 22:59:57 +0530

    Added comments for 13th day post

--- src/problems/Day13Problem_SearchA2DMatrix.java
+++ src/problems/Day13Problem_SearchA2DMatrix.java
@@ -1,5 +1,16 @@
 package problems;
 
+// Search a 2D Matrix
+/*
+   You are given an m x n integer matrix 'matrix' with the following two properties:
+   1. Each row is sorted in non-decreasing order.
+   2. The first integer of each row is greater than the last integer of the previous row.
+
+   Given an integer target, return true if target is in matrix or false otherwise.
+
+   You must write a solution in O(log(m * n)) time complexity.
+ */
+
 public class Day13Problem_SearchA2DMatrix {
 
     static  boolean searchMatrix(int[][] matrix, int target) {
@@ -8,6 +19,7 @@ public class Day13Problem_SearchA2DMatrix {
         while (l1 <= h1) {
             int m1 = (l1 + h1) / 2;
             int low = 0, high = matrix[m1].length - 1;
+            // checking whether the target lies in this row or not
             if (matrix[m1][low] <= target && matrix[m1][high] >= target) {
                 int l2 = 0, h2 = matrix[m1].length - 1;
                 while (l2 <= h2) {
@@ -23,6 +35,10 @@ public class Day13Problem_SearchA2DMatrix {
                 }
                 return false;
             } else if (matrix[m1][low] > target) {
+                // since each row is sorted, and it is given that the first integer of eah row
+                // is grater than the last integer of previous row, we can check through the above
+                // condition that whether out target is going to be there at the previous row corresponding
+                // to the current row or on the next row with respect to the current row
                 h1 = m1 - 1;
             } else  {
                 l1 = m1 + 1;
@@ -40,6 +56,14 @@ public class Day13Problem_SearchA2DMatrix {
                 {23, 30, 34, 60},
         };
         int target = 60;
+
+        // Input Case 02:
+        // int[][] matrix = {
+        //          {1, 3, 5, 7},
+        //          {10, 11, 16, 20},
+        //          {23, 30, 34, 60},
+        //  };
+        // int target = 8;
 
 
         boolean res = searchMatrix(matrix, target);
